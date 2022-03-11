09e54429d9ec2a6a9e654ab19c0879974b8a1298
N-yun <84072767+N-yun@users.noreply.github.com>
2022-03-11 16:57:55 +0900

    Initial commit
    a
    
    a

---
+++ src/main/java/com/example/strg/StrgApplication.java
@@ -0,0 +1,13 @@
+package com.example.strg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/strg/StrgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.strg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
